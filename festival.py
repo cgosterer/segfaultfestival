9b73681a072d086daaae9b742e8a037c2e72ce5e
cgosterer <cgo11@my.fsu.edu>
2018-11-29 23:04:00 -0800

    added pictures, registratioon is done, login works but for only one person for now, added basic searches for artists and festivals, more searches will come for zip code and band genre

--- festival.py
+++ festival.py
@@ -1,24 +1,29 @@
+import mysql.connector
+import random
+import time
+from hashlib import sha256 as userHash
 from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 from flask_table import Table, Col
+from flask_bcrypt import Bcrypt
+from mysql.connector.cursor import MySQLCursorPrepared
+
 import flask_whooshalchemy as wa
 
-
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:01Sda&hw@localhost/testdb'
-
-#tring to copnnect to gearhost database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://costerertestdb:Ik6N-wXcGo7_@den1.mysql6.gear.host/costerertestdb'
-
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 app.config['WHOOSH_BASE']='whoosh'
+app.config['SECRET_KEY'] = 'KJNF0128YURT08TN8G20TY0H0'          # can be ignored for now serves no purpose yet
+
 db = SQLAlchemy(app)						# final initialization step of the database db, db is now the database
+bcrypt = Bcrypt(app)
+
+cnx = mysql.connector.connect(user='costerertestdb', password='Ik6N-wXcGo7_', host='den1.mysql6.gear.host', database='costerertestdb', use_pure=True)
 
 
-app.config['SECRET_KEY'] = 'KJNF0128YURT08TN8G20TY0H0'		# can be ignored for now serves no purpose yet
 
 colors = ['red', 'blue', 'green', 'yellow']
 
@@ -48,19 +53,118 @@ posts = [						# most recent needs to be top post, announcement posts
         }
 ]
 
+#class User(db.Model):								# this probably wont be needed
+	#username = db.column(db.string(50), unique=True, nullable = False)
+	#email = db.column(db.string(255), unique=False, nullable = False)
+	#hash = db.column(db.string(64), unique=False, nullable = False)
+	#salt = db.column(db.Integer,unique= False, nullable=False)
+	#standing= db.column(db.Boolean,unqiue = False, nullable = False)
 
-class Results(Table):
-	persname = Col('persname')
-
-
-class testtable(db.Model):					# we will have classes for each table, testtable is a single table with a persname column
-	__tablename__ = 'testtable'
-	__searchable__ = ['persname']				# n ames of columns that will be searchable
-	persname = db.Column(db.String(255), primary_key=True)
-
-wa.whoosh_index(app, testtable) 					#Post is the name of the above class, this can be removed serves no purpose
+	#def __repr__(self):
+		#return f"User('{self.username}', '{self.email}')"
 
 
+class Results(Table):				# these classes are used to form tables to be displayed on webpage based on sql queries by web page users
+	artistsname = Col('artistsanme')
+	genre = Col('genre')
+	nummembers = Col('nummembers')
+
+class UserResults(Table):
+	username = Col('username')
+        email = Col('email')
+        standing = Col('standing')
+
+class bandResults(Table):
+	name = Col('name')
+        websiteURL = Col('websiteURL')
+        spotifyURL = Col('spotifyURL')
+	founded = Col('founded')
+	active = Col('active')
+
+class SongResults(Table):
+        songName = Col('songName')
+        bandName = Col('bandname')
+        album = Col('album')
+        runtime = Col('runtime')
+
+class festivalResults(Table):
+	name = Col('name')
+        startDate = Col('startDate')
+        location = Col('location')
+        websiteURL = Col('websiteURL')
+
+class festivalSchResults(Table):
+	festivalName = Col('festivalname')
+        festivalStart = Col('festivalStart')
+        bandName = Col('bandName')
+        performanceTime = Col('performanceTime')
+
+class favoritedSongs(Table):
+	user = Col('user')
+        song = Col('song')
+        band = Col('band')
+        album = Col('album')
+
+class BandModList(Table):
+        moderator = Col('moderator')
+        bandName = Col('bandName')
+
+class festModList(Table):
+        moderator = Col('moderator')
+        festivalName = Col('festivalName')
+
+class AdminList(Table):
+	username = Col('username')
+
+def hashSalt(password, salt):
+	saltedPassword = password + str(salt)
+	hasher = userHash(saltedPassword.encode('utf-8'))
+	h = hasher.hexdigest()
+	return h
+
+def createAccount(connection, username, password, email):
+	if(checkExists(connection, username) == True):
+		return False
+	cursor = connection.cursor(prepared=True)
+	statement = "INSERT INTO User(username, email, hash, salt, standing) VALUES(%s, %s, %s, %s, %s)"
+	maxint = 0xFFFFFFFFFFFFFFFF
+	random.seed(int(time.time() * 1000))
+	s = random.randint(0, maxint)
+	#print("Generated Salt", s)
+	h = hashSalt(password, s)
+	#print("The hash is", h)
+	cursor.execute(statement, (username, email, h, s, 1))
+	connection.commit()
+	cursor.close()
+	return True
+
+def checkPassword(connection, username, password):
+	cursor = connection.cursor(prepared=True)
+	select = "SELECT * FROM User WHERE username=%s AND hash=%s"
+	getHash = "SELECT salt FROM User WHERE username=%s"
+	cursor.execute(getHash, (username,))
+	salt = 0
+	for data in cursor:
+		salt = data[0]
+									#print("Grabbed salt: ", salt)
+	h = hashSalt(password, salt)
+									#print("The hash is", h)
+	cursor.execute(select, (username, h))
+	val = False
+	for data in cursor:
+		val = True
+	cursor.close()
+	return val
+
+def checkExists(connection, username):
+	cursor = connection.cursor(cursor_class=MySQLCursorPrepared)
+	statement = "SELECT * FROM User WHERE username=%s"
+	cursor.execute(statement, (username,))
+	val = False
+	for data in cursor:
+		val = True
+	cursor.close()
+	return val
 
 @app.route("/")
 @app.route("/Home")						# 127.0.0.1/Home
@@ -76,20 +180,48 @@ def contact():
         return render_template('contact.html', title = 'Contact Us')
 
 
-@app.route("/Concerts", methods=['GET'])
-def concerts():								# search these by: zip ....
-        return render_template('concerts.html', posts = posts, colors=colors )
+@app.route("/Festivals", methods=['GET', 'POST'])
+def festivals():								# search these by: zip ....
+	if request.method == 'POST':
+		#rows = testtable.query.whoosh_search(request.args.get('query')).all()          # this may not do what you think it does
+		#rows = testtable.query.all()
+		formtext = request.form['query']
+		#sqls = text('select * from testtable where persname="'+ formtext  +'";')                                       # text(<sequel query here>)
+		sqls = text('select * from artists;')                                       # text(<sequel query here>)
+		rows = db.engine.execute(sqls)                                                  # gets the rows that match the search
+		table = Results(rows)
+		#table = bandResults(rows)
+		table.border= True
+		return render_template('festivals.html', table=table, posts=posts, colors=colors)          # displays rows and the colors list localhost/search
+	else:
+		return render_template('festivals.html', posts=posts, colors=colors) 
 
-@app.route("/Artists")
-def artists():
-        return render_template('artists.html', title = 'About')
 
+@app.route("/Bands", methods= ['GET','POST'])
+def bands():
+	if request.method == 'POST':
+		#rows = testtable.query.whoosh_search(request.args.get('query')).all()          # this may not do what you think it does
+		#rows = testtable.query.all()
+		formtext = request.form['query']
+		#sqls = text('select * from testtable where persname="'+ formtext  +'";')                                       # text(<sequel query here>)
+		sqls = text('select * from artists;')                                       # text(<sequel query here>)
+		rows = db.engine.execute(sqls)                                                  # gets the rows that match the search
+		table = Results(rows)
+		#table = bandResults(rows)
+		table.border= True
+		return render_template('bands.html', table=table, posts=posts, colors=colors)          # displays rows and the colors list localhost/search
+	else:
+		return render_template('bands.html', posts=posts, colors=colors) 
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
 	form = RegistrationForm()
 	if form.validate_on_submit():
-		flash(("Account Created for {form_username_data}").format(form_username_data=form.username.data), 'success')
+		if(createAccount(cnx, form.username.data, form.password.data, form.email.data)):
+			flash('Your account has been created! You may now log in!', 'success')
+		else:
+			flash('Error Creating Account, Please Retry with Different Username', 'success')
+
 		return redirect(url_for('home'))					# redirect to home pg on succesful log in
 	return render_template('register.html', title='Register', form=form)
 
@@ -97,26 +229,14 @@ def register():
 def login():
 	form = LoginForm()
 	if form.validate_on_submit():
-		if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+		if (checkPassword(cnx, form.username.data, form.password.data)):
 			flash('YOU have been logged in', 'success')
 			return redirect(url_for('home'))				# redirect to home pg on succesful log in
 		else:
 			flash('Login Unsuccessful, Check username and password', 'danger')	# log in error
 	return render_template('login.html', title='Login', form=form)
 
-@app.route('/search' , methods=['GET', 'POST'])
-def search():
-	if request.method == 'POST':
-		#rows = testtable.query.whoosh_search(request.args.get('query')).all()		# this may not do what you think it does
-		#rows = testtable.query.all()
-		formtext = request.form['query']
-		sqls = text('select * from testtable where persname="'+ formtext  +'";')					# text(<sequel query here>)
-		rows = db.engine.execute(sqls)							# gets the rows that match the search
-		table = Results(rows)
-		table.border= True
-		return render_template('search.html', table=table, posts=posts, colors=colors)		# displays rows and the colors list localhost/search
-	else:
-		return render_template('search.html', posts=posts, colors=colors) 
+
 
 if __name__ == '__main__':								# main function
 	app.run(debug=True)
