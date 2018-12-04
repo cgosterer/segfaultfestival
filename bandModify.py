0096f82897e78aed3d9c32c7eaad978e3bc0b956
Steven Perez <scpatlantis79@gmail.com>
2018-12-03 19:50:19 -0500

    Added commit statements to modify functions

--- bandModify.py
+++ bandModify.py
@@ -5,39 +5,46 @@ def addSong(connection, bandName, songName, album):
     statement = "INSERT INTO Song(songName, bandName, album) VALUES(%s, %s, %s);"
     cursor.execute(statement, (songName, bandName, album))
     cursor.close()
+    connection.commit()
 
 def removeSong(connection, bandName, songName, album):
     cursor = connection.cursor(prepared=True)
     statement = "DELETE FROM Song WHERE songName=%s AND bandName=%s AND album=%s;"
     cursor.execute(statement, (songName, bandName, album))
     cursor.close()
+    connection.commit()
 
 def updateFoundingDate(connection, band, date):
     cursor = connection.cursor(prepared=True)
     statement = "UPDATE Band SET founded=%s WHERE bandName=%s;"
     cursor.execute(statement, (date, band))
     cursor.close()
+    connection.commit()
 
 def setActive(connection, band):
     cursor = connection.cursor(prepared=True)
     statement = "UPDATE Band SET active=True WHERE bandName=%s;"
     cursor.execute(statement, (band,))
     cursor.close()
+    connection.commit()
 
 def setActive(connection, band):
     cursor = connection.cursor(prepared=True)
     statement = "UPDATE Band SET active=False WHERE bandName=%s;"
     cursor.execute(statement, (band,))
     cursor.close()
+    connection.commit()
 
 def addMod(connection, band, user):
     cursor = connection.cursor(prepared=True)
     statement = "INSERT INTO BandModeratorList(moderator, bandName) VALUES(%s, %s);"
     cursor.execute(statement, (user, band))
     cursor.close()
+    connection.commit()
 
 def removeMod(connection, band, user):
     cursor = connection.cursor(prepared=True)
     statement = "DELETE FROM BandModeratorList WHERE bandName=%s AND moderator=%s"
     cursor.execute(statement, (band, user))
     cursor.close()
+    connection.commit()
